cc65566b4e91ec1d7811ee6e7e5c2b8f2de4bca3
Soleil <shengyang998@gmail.com>
2020-08-09 01:50:33 +0800

    Initial Commit

---
+++ ScreenColorTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ScreenColorTest
+//
+//  Created by Soleil Yu on 2020/8/9.
+//  Copyright © 2020 Soleil Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
